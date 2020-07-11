261b50121cf1d7d15b1af95302a4bcb48c13c7f9
Alexis La Goutte <alexis.lagoutte@gmail.com>
2020-07-11 08:25:11 +0200

    SSL: Add Set-ArubaCLCipherSSL
    for allow TLS 1.2 when using PS 5

---
+++ PowerArubaCL/Private/SSL.ps1
@@ -0,0 +1,14 @@
+#
+# Copyright 2020, Alexis La Goutte <alexis.lagoutte at gmail dot com>
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+function Set-ArubaCLCipherSSL {
+  [Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSUseShouldProcessforStateChangingFunctions", "")]
+  Param(  )
+  # Hack for allowing TLS 1.1 and TLS 1.2 (by default it is only SSL3 and TLS (1.0))
+  $AllProtocols = [System.Net.SecurityProtocolType]'Ssl3,Tls,Tls11,Tls12'
+  [System.Net.ServicePointManager]::SecurityProtocol = $AllProtocols
+
+}
