c3a44d466c4deba2d12412e7aebb40a74ef9863d
Alexis La Goutte <alexis.lagoutte@gmail.com>
2021-09-13 18:09:12 +0200

    Token: Add Timeout attribute
    and enhance example

--- PowerArubaCL/Private/Token.ps1
+++ PowerArubaCL/Private/Token.ps1
@@ -47,16 +47,25 @@ function Get-ArubaCLTokenStatus {
 
     .EXAMPLE
     Get-ArubaCLTokenStatus
+
+    Get token status (expired or not)
+
+    .EXAMPLE
+    Get-ArubaCLTokenStatus -timeout 900
+
+    Get token status (expired or not) on less of 900 seconds (15 minutes)
     #>
 
     Param(
+        [Parameter(Mandatory = $false)]
+        [int]$timeout = 0,
         [Parameter(Mandatory = $false)]
         [psobject]$connection = $DefaultArubaCLConnection
     )
 
     $expire = $connection.token.expire
     $now = [int]((Get-Date -UFormat %s) -split ",")[0]
-    if (($expire - $now) -le 0) {
+    if (($expire - $now) -le $timeout) {
         # If token is expired, it should fail boolean
         return $false
     }
